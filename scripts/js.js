d9b2d7e7a2f437c3373501b1b54665ddb4dbf53b
El Giorgio <jruanova1987@gmail.com>
2022-08-28 19:07:18 -0300

    desafio 1 - corrections

--- scripts/js.js
+++ scripts/js.js
@@ -1,40 +1,51 @@
-const productQuantity = parseInt(prompt("Ingrese cantidad de productos a registrar:"))
+let productQuantity = parseInt(prompt("Ingrese cantidad de productos a registrar:"))
+
+while ((productQuantity <= 0) || (!productQuantity)) {
+    productQuantity = parseInt(prompt("El valor debe ser mayor a 0 y numerico"))
+}
 let totalAmount = 0
 
+
+// ---------------------------------------------------------------------------
+
 for (let index = 1; index <= productQuantity; index++) {
-    const productOrder = parseFloat(prompt("Ingrese el valor del primer producto " + index))
+    let productOrder = parseFloat(prompt("Ingrese el valor del primer producto " + index))
+    while ((productOrder <= 0) || (!productOrder)) {
+        productOrder = parseInt(prompt("El valor debe ser mayor a 0 y numerico"))
+    }
     totalAmount = totalAmount + productOrder
 }
+alert("El total de su pedido es: \nTotal de arituclos registrados: " + productQuantity + "\nTotal a abonar: $" + totalAmount)
 
-if (!productQuantity) {
-    alert('Debe ingresar al menos 1 producto')
+// ---------------------------------------------------------------------------
+
+
+let payment = parseInt(prompt("Ingrese el total a abonar para poder calcular su vuelto:"))
+
+while ((payment < 0 || payment != payment)) {
+    payment = parseInt(prompt("Debe ingresar al menos un valor y debe ser mayor o igual a 0"))
+
 }
-else if (productQuantity == 0) {
-    alert('Debe ingresar al menos 1 producto')
+const paymentDif = totalAmount - payment
+if (!payment) {
+    alert('Usted no ha abonado. Adeuda el total: $' + totalAmount)
+}
+else if (paymentDif == 0) {
+    alert('Su cuenta ha sido saldada. \nGracias.')
+} else if (paymentDif < 0) {
+    alert("Usted tiene un saldo acreedor de $" + Math.abs(paymentDif) + ". \nGracias.")
 } else {
-    alert("El total de su pedido es: \nTotal de arituclos registrados: " + productQuantity + "\nTotal a abonar: $" + totalAmount)
+    alert("Lamentablemente el monto no ha sido cubierto. \nUsted todavia debe abonar un total de $" + paymentDif + ". \nGracias.")
 }
 
 
-if (!productQuantity) {
-    ""
-} else if (productQuantity == 0) {
-    ""
-} else {
-    const payment = parseInt(prompt("Ingrese el total a abonar para poder calcular su vuelto:"))
-    const paymentDif = totalAmount - payment
-    if (!payment) {
-        alert('Usted no ha abonado. Adeuda el total: $' + totalAmount)
-    }
-    else if (paymentDif == 0) {
-        alert('Su cuenta ha sido saldada. \nGracias.')
-    } else if (paymentDif < 0) {
-        alert("Usted tiene un saldo acreedor de $" + Math.abs(paymentDif) + ". \nGracias.")
-    } else {
-        alert("Lamentablemente el monto no ha sido cubierto. \nUsted todavia debe abonar un total de $" + paymentDif + ". \nGracias.")
-    }
+// ---------------------------------------------------------------------------
+console.log("Total productos= $" + productQuantity)
+console.log("Total a a pagar= $" + totalAmount)
+console.log("Total del pago= $" + payment)
+console.log("Diferencia= $" + (totalAmount - payment))
+// ---------------------------------------------------------------------------
 
-}
 
 let newRequest = prompt("Algo mas en que lo pueda ayudar?\n(pista: elegir 'a' para finalizar)\n a. No gracias.\nb. Dejame pensar.\nv. No estoy seguro.")
 while (newRequest != "a") {
