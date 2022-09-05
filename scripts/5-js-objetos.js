54c9fa8bd9d9421af997e30f5750c6eb24b014a1
El Giorgio <jruanova1987@gmail.com>
2022-09-05 20:20:20 -0300

    objects test

--- scripts/5-js-objetos.js
+++ scripts/5-js-objetos.js
@@ -1,3 +1,4 @@
+
 // MESSAGES: PRODUCTS QUANTITY---
 const productQuantityText = 'Ingrese cantidad de productos a registrar:'
 const errorInputMessage = 'Ingrese cantidad de productos a registrar.\n(el valor debe ser mayor a 0 y numerico)'
@@ -10,9 +11,19 @@ const errorInputValueMessage = 'Ingrese el valor del producto.\n(el valor debe s
 const paymentMessage = 'Ingrese el total a abonar para poder calcular su vuelto:'
 const paymentErrorMessage = 'Debe ingresar al menos un valor y debe ser mayor o igual a 0\n (por lo menos $1 trata de pagar)'
 
-//SHIPPING MESSAGE
-const shippingError= 'Debe ingresar al menos un valor'
+
+
 // ---------------------------------------------------------------------------------------------
+
+
+function capitalizedText(value1) {
+    let capitalized = ""
+    capitalized = value1.charAt(0).toUpperCase() + value1.slice(1)
+    return capitalized;
+}
+
+
+
 
 // FUNCTION MESSAGES
 function messageGenerator(message, value, messageselector) {
@@ -56,13 +67,70 @@ function whileAutomation(var1, var2, var3, var4) {
 
 
 
+
+
+
 //  FUNCTION PRINT DETAIL
 function printBilling() {
     console.log("Total productos: " + productQuantity + " - " + "Total a a pagar: $" + totalAmount.toLocaleString('en-US'))
     console.log('----------------------------------------')
     console.log("Total del pago: $" + payment.toLocaleString('en-US') + " - " + dato + " $" + Math.abs(totalAmount - payment).toLocaleString('en-US'))
+    console.log('-------------------|---------------------')
+}
+
+//  FUNCTION PRINT SHIPPING
+
+function printShipping() {
+    const shippingOne = shippingQuestions();
+    console.log("Sus productos (" + productQuantity + ") por un total de $" + totalAmount.toLocaleString('en-US') + "\nSeran entregados en la direccion: \n" + capitalizedText(shippingOne.address) + " " + shippingOne.number + "\n" + capitalizedText(shippingOne.city) + ", " + (shippingOne.country).toUpperCase() + "\n(CP: " + shippingOne.zipcode + ")");
+
+    alert("Sus productos (" + productQuantity + ") por un total de $" + totalAmount.toLocaleString('en-US') + "\nseran entregados en la direccion: \n" + capitalizedText(shippingOne.address) + " " + shippingOne.number + "\n" + capitalizedText(shippingOne.city) + ", " + (shippingOne.country).toUpperCase() + "\n(CP: " + shippingOne.zipcode + ")")
+}
+
+
+// OBJECT CREATION------------------------------
+function shippingAddress(address, number, city, country, zipcode) {
+    this.address = address;
+    this.number = number;
+    this.city = city;
+    this.country = country;
+    this.zipcode = zipcode;
 
 }
+
+function whileShippingText(var1) {
+    while ((!var1)) {
+        var1 = prompt(("Debe ingresar al menos un dato"))
+    }
+    return var1
+}
+function whileShippingNumber(var1) {
+    while ((!var1) || (var1 <= 0)) {
+        var1 = parseInt(prompt(("Debe ingresar al menos una numeracion")))
+    }
+    return var1
+}
+
+
+function shippingQuestions() {
+    let address = prompt("Ingrese su direccion");
+    address = whileShippingText(address);
+    let number = prompt("Ingrese la numeracion");
+    number = whileShippingNumber(number);
+    let city = prompt("Ingrese la ciudad");
+    city = whileShippingText(city);
+    let country = prompt("ingrese el pais");
+    country = whileShippingText(country);
+    let zipcode = parseInt(prompt("Ingrese su CP"));
+    zipcode = whileShippingNumber(zipcode);
+
+    const newShipping = new shippingAddress(
+        address, number, city, country, zipcode
+    );
+    return newShipping;
+    ;
+}
+
 
 
 // PROD QUANTITY---------------------------------------------------------------------------------------------
@@ -92,21 +160,31 @@ let dato = ""
 if (!payment) {
     alert('Usted no ha abonado. El envio no puede completarse. Adeuda el total: $' + totalAmount.toLocaleString('en-US'))
     dato = 'Saldo deudor de'
+    printBilling()
 }
 else if (paymentDif == 0) {
     alert('Su cuenta ha sido saldada.\nA continuacion procederemos a generar el envio del producto')
     dato = 'Su cuenta esta en'
+    printBilling()
+    printShipping()
 
 } else if (paymentDif < 0) {
     alert("Su cuenta ha sido saldada.\nA continuacion procederemos a generar el envio del producto.\n(usted tiene un saldo acreedor de $" + Math.abs(paymentDif).toLocaleString('en-US') + ".")
     dato = 'Saldo acreedor de'
+    printBilling()
+    printShipping()
+
 
 } else {
     alert("Lamentablemente el monto no ha sido cubierto.\nEl envio no puede completarse.  \nUsted todavia debe abonar un total de $" + paymentDif.toLocaleString('en-US') + ". \nGracias.")
     dato = 'Saldo pendiente de'
+    printBilling()
 }
 
 
 
 
 //---------
+
+
+
